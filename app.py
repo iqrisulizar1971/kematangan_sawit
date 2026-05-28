ca69103a58c4444b23a4e01635ccbaa579cd27a0
iqrisulizar1971 <iqrisulizar@socialinvestment.id>
2026-05-28 11:42:08 +0700

    perbaikan tambah nama pembuat4

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ with st.sidebar:
 st.markdown("""
 <div class="main-header">
   <h1>🌴 Otomasi Deteksi Kematangan Buah Sawit</h1>
-  <p>Iqri Sulizar Hidriansjah · Kontak WA: 0852-1939-0680 . Email:iqrisulizar@gmail.com</p>
+  <p>Iqri Sulizar Hidriansjah · Kontak WA: 0852-1939-0680 . Email: iqrisulizar@gmail.com . LinkedIn: linkedin.com/in/iqrisulizar</p>
 </div>
 """, unsafe_allow_html=True)
 
